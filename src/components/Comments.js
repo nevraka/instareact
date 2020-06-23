fbb7eb9d691cfeb8d219fa7c709abdad1da6f97f
nevraka <nevrakaya@gmail.com>
2020-06-23 22:28:26 +0200

    toggle show all comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./comment_icon.css";
 
 const Comments = (props) => {
+  const [showAllComments, setShowAllComments] = useState(true);
+  const onToogleShowAllComments = () => setShowAllComments(!showAllComments);
+
   return (
     <>
       {props.comments.length === 0 ? (
         <div className={"add-comment"} onClick={props.onToggleShowInput}>
           Add Comments
         </div>
-      ) : (
+      ) : showAllComments ? (
         <>
           {props.comments.map((c) => {
             return <div>{c}</div>;
           })}
-          <div onClick={props.onToggleShowInput}>View all comments</div>
+          <div onClick={onToogleShowAllComments}>Hide comments</div>
         </>
+      ) : (
+        <div onClick={onToogleShowAllComments}>View all comments</div>
       )}
     </>
   );
