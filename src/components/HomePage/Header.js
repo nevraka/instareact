3c3d993d951dd3f9d49b799b622a2a9a91d0057c
nevraka <nevrakaya@gmail.com>
2020-07-09 16:30:15 +0200

    use react router dom links

--- src/components/HomePage/Header.js
+++ src/components/HomePage/Header.js
@@ -1,14 +1,13 @@
-import React, { useContext } from "react";
-import logo from "../../image/logo.png";
-import "./header.css";
-import SearchBox from "./SearchBox";
-import LoginButton from "./LoginButton";
-import UserContext from "../../context/UserContext";
-import { useHistory } from "react-router-dom";
+import React, { useContext } from 'react';
+import logo from '../../image/logo.png';
+import './header.css';
+import SearchBox from './SearchBox';
+import LoginButton from './LoginButton';
+import UserContext from '../../context/UserContext';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const user = useContext(UserContext);
-  const history = useHistory();
 
   return (
     <h2 className="ui-header">
@@ -16,13 +15,12 @@ const Header = () => {
         <img className="logo" src={logo} alt="" />
         <SearchBox />
         <div className="icon-header">
-          <i
-            className="home icon"
-            onClick={() => {
-              history.push("/");
-            }}
-          />
-          <i className="paper plane outline icon"></i>
+          <Link to="/">
+            <i className="home icon" />
+          </Link>
+          <Link to="/message">
+            <i className="paper plane outline icon" />
+          </Link>
           <i className="compass icon"></i>
           <i className="heart icon"></i>
         </div>
