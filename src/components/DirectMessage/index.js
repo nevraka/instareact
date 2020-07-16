8a4e3691c0a402faaa85325e5eda40de78fc7aa9
nevraka <nevrakaya@gmail.com>
2020-07-16 23:47:46 +0200

    direct message design

--- src/components/DirectMessage/index.js
+++ src/components/DirectMessage/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import './index.css';
+import '../HomePage/Timeline';
+import PeopleList from './PeopleList';
 
 const DirectMessage = () => {
   return (
-    <div>
-      <div class="ui small message" />
-      This is a small message.
-      <div class="ui small message" />
-      This is a small message.
+    <div className="message-box">
+      <div className="ui two column grid direct">
+        <div className="six wide column">
+          <div className="ui vertical fluid menu">
+            <div className="header item">
+              Direct
+              <i class="edit outline icon"></i>
+            </div>
+          </div>
+          <div>
+            <PeopleList />
+          </div>
+        </div>
+        <div className="ten wide column">
+          <div className="ui vertical fluid menu"></div>
+        </div>
+      </div>
     </div>
   );
 };
