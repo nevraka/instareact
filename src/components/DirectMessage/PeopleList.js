afd261c852bcfcda71f115c2fc0f6108787ee498
nevraka <nevrakaya@gmail.com>
2020-07-18 23:45:51 +0200

    use current user

--- src/components/DirectMessage/PeopleList.js
+++ src/components/DirectMessage/PeopleList.js
@@ -1,20 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './index.css';
-import unsplash from '../../api/unsplash';
 
-const PeopleList = () => {
-  const [users, setUsers] = useState(null);
-
-  useEffect(() => {
-    const callAPI = async () => {
-      const result = await unsplash.get(
-        `/search/photos?query=people&per_page=20`
-      );
-      setUsers(result.data.results);
-    };
-    callAPI();
-  }, []);
-
+const PeopleList = ({ users }) => {
   return (
     <div className="list-ui">
       <div class="ui middle aligned divided list">
@@ -22,13 +9,15 @@ const PeopleList = () => {
           users.map((user) => {
             return (
               <div class="item">
-                <div class="right floated content"></div>
-                <img
-                  class="ui avatar image"
-                  src="/images/avatar2/small/lena.png"
-                  alt=""
-                />
-                <div class="content">{user.user.name}</div>
+                <div>
+                  <div class="right floated content"></div>
+                  <img
+                    src={user.profile_image.medium}
+                    alt=""
+                    className="user-image"
+                  />
+                  <div className="content user-name">{user.name}</div>
+                </div>
               </div>
             );
           })}
--- src/components/DirectMessage/index.js
+++ src/components/DirectMessage/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import '../HomePage/Timeline';
 import PeopleList from './PeopleList';
+import unsplash from '../../api/unsplash';
 
 const DirectMessage = () => {
+  const [users, setUsers] = useState(null);
+  let currentUser = null;
+  if (users && users.length > 0) {
+    currentUser = users[0];
+  }
+
+  useEffect(() => {
+    const callAPI = async () => {
+      const result = await unsplash.get(
+        `/search/photos?query=people&per_page=20`
+      );
+      setUsers(result.data.results.map((item) => item.user));
+    };
+    callAPI();
+  }, []);
+
   return (
     <div className="message-box">
       <div className="ui two column grid direct">
@@ -15,11 +32,13 @@ const DirectMessage = () => {
             </div>
           </div>
           <div>
-            <PeopleList />
+            <PeopleList users={users} />
           </div>
         </div>
         <div className="ten wide column">
-          <div className="ui vertical fluid menu"></div>
+          <div className="ui vertical fluid menu">
+            {currentUser && currentUser.name}
+          </div>
         </div>
       </div>
     </div>
