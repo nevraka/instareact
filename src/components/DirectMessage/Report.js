d74f68a50328cf2ffc7ecc8ede9f02e6957264b9
nevraka <nevrakaya@gmail.com>
2020-07-24 17:47:24 +0200

    css changed

--- src/components/DirectMessage/Report.js
+++ src/components/DirectMessage/Report.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
-const Report = ({ setShowReport }) => {
+const Report = ({ setShowReport, currentUserName }) => {
   return (
-    <div>
+    <div className="report">
+      <h3 className="ui dividing header">
+        Report
+        <i
+          className="x icon x-icon"
+          onClick={() => {
+            setShowReport(false);
+          }}
+        ></i>
+      </h3>
       <div>
-        <h3 class="ui top attached header report-header">
-          Report
-          <i
-            class="x icon"
-            onClick={() => {
-              setShowReport(false);
-            }}
-          ></i>
-        </h3>
+        Choose a reason for reporting this account. We won't tell&nbsp;
+        {currentUserName}&nbsp;who reported them.
+      </div>
+      <div className="ui relaxed list report-options">
+        <a className="item">I just don't like it</a>
+        <a className="item">It's spam</a>
+        <a className="item">Nudity or pornography</a>
+        <a className="item">Hate speech or symbol</a>
+        <a className="item">Other</a>
       </div>
     </div>
   );
