d58d770b16079f711059ed3d1122a01639d0231c
nevraka <nevrakaya@gmail.com>
2020-07-24 17:46:49 +0200

    block and report modal function added

--- src/components/DirectMessage/ConversationDetail.js
+++ src/components/DirectMessage/ConversationDetail.js
@@ -3,39 +3,71 @@ import './index.css';
 import avatar from '../../image/avatar.png';
 import Modal from 'react-modal';
 import Report from './Report';
+import Block from './Block';
 
 const ConversationDetail = ({ currentUser }) => {
   const [showReport, setShowReport] = useState(false);
+  const [showBlock, setShowBlock] = useState(false);
+
+  const closeModalReport = () => setShowReport(false);
+  const closeModalBlock = () => setShowBlock(false);
 
   return (
-    <div className="ui checkbox show-details">
-      <input type="checkbox" name="example" />
-      <label>Mute Messages</label>
-      <div className="details-box">
-        <div>Members </div>
-        <img
-          className="details-image"
-          src={currentUser ? currentUser.profile_image.medium : avatar}
-          alt=""
-        />
-        {currentUser && currentUser.name}
-        <div class="ui list">
-          <a class="item">Delete Chat</a>
-          <a class="item">Block</a>
-          <a
-            class="item"
-            onClick={() => {
-              setShowReport(true);
-            }}
-          >
-            Report
-          </a>
+    currentUser && (
+      <div className="ui checkbox show-details">
+        <input type="checkbox" name="example" />
+        <label>Mute Messages</label>
+        <div className="details-box">
+          <div>Members </div>
+          <img
+            className="details-image"
+            src={currentUser ? currentUser.profile_image.medium : avatar}
+            alt=""
+          />
+          {currentUser.name}
+          <div class="ui list">
+            <a class="item">Delete Chat</a>
+            <a
+              class="item"
+              onClick={() => {
+                setShowBlock(true);
+              }}
+            >
+              Block
+            </a>
+            <a
+              class="item"
+              onClick={() => {
+                setShowReport(true);
+              }}
+            >
+              Report
+            </a>
+          </div>
         </div>
+        <Modal
+          className="modal"
+          style={{ overlay: { backgroundColor: '#00000088' } }}
+          shouldFocusAfterRender={false}
+          isOpen={showReport}
+          onRequestClose={closeModalReport}
+        >
+          <Report
+            setShowReport={setShowReport}
+            currentUserName={currentUser.name}
+          />
+        </Modal>
+        <Modal
+          className="modal"
+          style={{ overlay: { backgroundColor: '#00000088' } }}
+          shouldFocusAfterRender={false}
+          isOpen={showBlock}
+          onRequestClose={closeModalBlock}
+        >
+          <Block currentUserName={currentUser.name} />
+        </Modal>
       </div>
-      <Modal isOpen={showReport}>
-        <Report setShowReport={setShowReport} />
-      </Modal>
-    </div>
+    )
   );
 };
 
