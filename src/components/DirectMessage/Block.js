e9df48b08edf5c798211db1c85c0b6d274ca5d0d
nevraka <nevrakaya@gmail.com>
2020-07-27 15:46:52 +0200

    changed some css

--- src/components/DirectMessage/Block.js
+++ src/components/DirectMessage/Block.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-const Block = ({ currentUserName }) => {
+const Block = ({ currentUserName, closeModalBlock }) => {
   return (
-    <div>
-      <div class="ui dividing header">Block&nbsp;{currentUserName}&nbsp;?</div>
+    <div className="block-page">
+      <div class="ui dividing header block-header">
+        Block&nbsp;{currentUserName}&nbsp;?
+      </div>
       <div>
         They won't be able to find your profile, posts or story on Instagram.
         Instagram won't let them know you blocked them.
       </div>
-      <div class="ui middle aligned divided list">
-        <div class="item">Block</div>
-        <div class="item">Cancel</div>
+      <div class="ui middle aligned divided list block-list">
+        <button class="ui basic button">Block</button>
+        <button class="ui basic button" onClick={closeModalBlock}>
+          Cancel
+        </button>
       </div>
     </div>
   );
